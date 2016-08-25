6ef553b7da7fc029dbd72682e6b032cd02780ae3
x95102003 <x95102003@gmail.com>
2016-08-25 16:46:59 +0800

    Modified with collapse panel to show more info

--- crawl.py
+++ crawl.py
@@ -37,12 +37,13 @@ class PTT_CRAWL(Thread):
         soup = bs(res.text.encode('utf-8'), "html.parser")
         last_page = str(soup.select('.btn')[3].get('href'))
         m = search('.*index([0-9]*).html', last_page)
-        last_pgnum = int(m.group(1))+1
-        return last_pgnum
+        last_num = int(m.group(1))+1
+        return last_num
         
     def crawl_assign(self, pgfrom, pgto):
-        self.last_pgnum = pgto+1
-        for i in xrange(pgfrom, pgto+1):
+        self.last_pgnum = pgto+2
+        for i in xrange(pgfrom, pgto+2):
+            print "Page num:%d" %i
             self.crawl(i)
 
     def crawl(self, index=None):
@@ -53,26 +54,22 @@ class PTT_CRAWL(Thread):
             res = get(url)
             res.raise_for_status()
             soup = bs(res.text.encode('utf-8'), "html.parser")
-            for line in soup.select('.r-ent'):
-                title_div = line.select('.title')[0]
-                link = self.base+title_div.a['href']
-                title = title_div.a.text.encode('utf-8')
-                price = self.filter_title(title, link)
-                #print title, link, price
-                if price and not title in self.collect_dict:
-                    self.collect_dict.update({title:[price,link]})
+            for title_div in soup.select('.title'):
+                if title_div.a:
+                    link = self.base+title_div.a['href']
+                    title = title_div.a.text.encode('utf-8')
+                    content= self.filter_title(title, link)
+                    if content and not title in self.collect_dict:
+                        self.collect_dict.update({title:[content[0],link, content[1]]})
                     #record(title, link, price)
         except HTTPError:
             print "No New board"
-        except TypeError as e:
-            pass
         except ConnectionError:
             pass
 
     def show_collect(self):
         sort_product = sorted(self.collect_dict.items(), key=lambda x:x[1][0]) 
         for tup in sort_product:
-            #key, value = tup
             print "{0: <50}\t\t{1}\t\t{2}".format(tup[0], tup[1][0], tup[1][1].decode('utf-8'))
         return sort_product
 
@@ -81,44 +78,41 @@ class PTT_CRAWL(Thread):
             tmp_pgnum= self.get_lastpage()
             print tmp_pgnum, self.last_pgnum
             if self.last_pgnum < tmp_pgnum:
-                self.crawl_assign( self.last_pgnum,tmp_pgnum )
+                self.crawl_assign( self.last_pgnum,tmp_pgnum)
                 print "New page found"
             else:
                 print "No new page"
                 self.crawl()
             sleep(120)
-            print self.collect_dict
+           # print self.collect_dict
 
     def get_price(self, url):
         res = get(url)
         soup = bs(res.text.encode('utf-8'), "html.parser")
         content = soup.select('.bbs-screen')[0].text.encode('utf-8')
-        #m = search(u'\u50f9\u683c[^0-9]*([0-9]*,?[0-9]*)',content.decode('utf-8'))
         price_list = findall(u'\u50f9\u683c[^0-9]*([0-9]*,?[0-9]*)',content.decode('utf-8')) 
         if price_list:
             price_list = map(lambda i:int(i.strip('u').encode('utf-8').replace(',','')), price_list)
-            check_price = lambda x:True if x >= 3000 and x <= 14000 else False
+            check_price = lambda x:True if x >= 4200 and x <= 14000 else False
             if len(price_list)>=2 and check_price(price_list[1]):
-                return price_list[1]
+                return [price_list[1], content]
             elif check_price(price_list[0]):
-                return price_list[0]
-#        if m:
-#            print "---%s---%d" %(m.group(), len(m.group()))
-#            price = int(m.group(1).replace(',',''))
-#            if price > 3000 and price < 14000:
-#                return price
-            
+                return [price_list[0], content]
+            else:
+                return None
     def filter_title(self, title, url):
-        if 'iphone' in title.lower():
+        #print title
+        if 'iphone' in title.lower() and '徵' not in title \
+            and '售出' not in title:
             return self.get_price(url)
 
 def main():
     global for_th
     global ms_th
-    for_th = PTT_CRAWL('forsale', 3200)
+    for_th = PTT_CRAWL('forsale', 3500)
     for_th.setDaemon(True)
     for_th.start()
-    ms_th = PTT_CRAWL('mobilesales', 11358)
+    ms_th = PTT_CRAWL('mobilesales', 11320)
     ms_th.setDaemon(True)
     ms_th.start()
     mth = Thread(target=monitor)
@@ -129,10 +123,10 @@ def monitor():
     while(True):
         num = raw_input("Input 1 to show collect\n")
         if num == '1':
-            print ms_th.show_collect()
+            ms_th.show_collect()
             #print ms_th
         elif num =='2':
-            print for_th.show_collect()
+            for_th.show_collect()
         else:
             pass
 
@@ -147,6 +141,6 @@ def show_data():
 
 if __name__ == '__main__':
     main()
-    #app.debug=True
+    app.debug=True
     app.run(host='0.0.0.0', port=8888)
 
